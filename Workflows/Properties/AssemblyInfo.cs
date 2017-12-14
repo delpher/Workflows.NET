6f275c0727b3b89d6d8648c1459da199d7c9d23f
Alexey <delph.er@hotmail.com>
2017-12-14 22:04:03 +0200

    Removed unnecessary files

--- Workflows/Properties/AssemblyInfo.cs
+++ Workflows/Properties/AssemblyInfo.cs
@@ -4,9 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("18122AFF-FF13-42DD-82CA-E0A84CB75F67")]
 
-[assembly: InternalsVisibleTo("Workflows.Tests, PublicKey=" +
-                              "0024000004800000940000000602000000240000525341310004000001000100f5a0235bf03e91"+
-                              "58bfc58ce3e2b2794773865af38fe06479cb0cc2b7d6671698b4fabf12979b8299a4886beff2c7"+
-                              "adcd882a2be97818d40265099516f05a5e7ae177530886cc823eb8f307c8732851aabbe8b0cbe0"+
-                              "82f46db2ca9bb17373b1326b5f20d2eba9c341511a26fe244f4f4a0893f01e4f71098760617c7e"+
-                              "7f8643c9")]
+[assembly: InternalsVisibleTo("Workflows.Tests")]
